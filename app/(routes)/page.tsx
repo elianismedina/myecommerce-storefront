41752751d21781bac55db7afa2cd58b339f7b3bd
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-11-07 11:53:31 -0500

    Add billboard component to homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,16 @@
+import billboard from "@/components/ui/billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
 
 export default HomePage;
