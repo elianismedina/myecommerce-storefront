6883a9c114008faf02f158c22a73116463ee636b
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-11-07 10:24:27 -0500

    Update MainNav component to use categories data

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -11,7 +11,7 @@ interface MainNavProps {
 
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
-  const routes = data?.map((route) => ({
+  const routes = data.map((route) => ({
     href: `/category/${route.id}`,
     label: route.name,
     active: pathname === `/category/${route.id}`,
@@ -19,7 +19,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes?.map((route) => (
+      {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
