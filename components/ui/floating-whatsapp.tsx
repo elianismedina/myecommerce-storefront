6a5e1d60bd6080a107325beaebfbd445af73487c
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-11-11 12:22:13 -0500

    Add WhatsApp support and update button text in Info component

---
+++ components/ui/floating-whatsapp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+export default function FloatingWhatsapp() {
+
+  return (
+      <FloatingWhatsApp {/*  Props  */} />
+  )
+}
