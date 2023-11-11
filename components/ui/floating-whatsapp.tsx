a70f7730550884e5ae7454ce694fd07282317aee
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-11-11 12:24:10 -0500

    deleted floating whatsapp component

--- components/ui/floating-whatsapp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-
-export default function FloatingWhatsapp() {
-
-  return (
-      <FloatingWhatsApp {/*  Props  */} />
-  )
-}
