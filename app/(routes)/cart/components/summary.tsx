4ffddd9a64fe5b7328f5c79163d1e3669ebb3087
crenacernegro <crenacernegro@gmail.com>
2024-01-16 14:40:26 -0500

    reversed changes

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
@@ -7,7 +8,6 @@ import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
-import OnCheckout from "./oncheckout";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -29,23 +29,32 @@ const Summary = () => {
     return total + Number(item.price);
   }, 0);
 
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+
+    window.location = response.data.url;
+  };
+
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Resumen de tu orden</h2>
+      <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">
-            Total de tu orden
-          </div>
+          <div className="text-base font-medium text-gray-900">Order total</div>
           <Currency value={totalPrice} />
         </div>
       </div>
       <Button
-        onClick={() => OnCheckout()}
+        onClick={onCheckout}
         disabled={items.length === 0}
         className="w-full mt-6"
       >
-        Finalizar compra
+        Checkout
       </Button>
     </div>
   );
