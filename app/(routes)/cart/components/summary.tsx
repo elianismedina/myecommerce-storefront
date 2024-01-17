4146a150940362573d9b51f8c287b38b96c56338
crenacernegro <crenacernegro@gmail.com>
2024-01-17 09:48:20 -0500

    changed summary

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -8,7 +8,6 @@ import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
-import Checkout from "./checkout";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -30,6 +29,17 @@ const Summary = () => {
     return total + Number(item.price);
   }, 0);
 
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+
+    window.location = response.data.url;
+  };
+
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
@@ -40,11 +50,11 @@ const Summary = () => {
         </div>
       </div>
       <Button
-        onClick={() => Checkout()}
+        onClick={onCheckout}
         disabled={items.length === 0}
         className="w-full mt-6"
       >
-        Finalizar compra
+        Checkout
       </Button>
     </div>
   );
