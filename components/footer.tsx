63fd249d07b2fb32fef0fab800d595a8534d1048
crenacernegro <crenacernegro@gmail.com>
2024-02-04 11:29:19 -0500

    changed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,8 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black font-bold">
-          &copy; 2023 A&E cueros S.A.S. Todos los derechos reservados.
-          Notificaciones judiciales Cr 71 # 37-350 ITAGUI - ANTIOQUIA 3008828214
-          | NIT 901673796-1
+          &copy; 2023 A&E cueros S.A.S. Todos los derechos reservados. NIT
+          901673796-1
         </p>
       </div>
     </footer>
